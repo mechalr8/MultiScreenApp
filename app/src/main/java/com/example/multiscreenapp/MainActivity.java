548310d5b6b90f865c3b54faeb9168d74dbdf17a
Harsit Agrawal <harsitagrawal59@gmail.com>
2021-11-29 20:49:48 +0530

    First commit

---
+++ app/src/main/java/com/example/multiscreenapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.multiscreenapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String MSG = "com.example.multiscreenapp.ORDER";
+    public void placeOrder(View view){
+        Intent intent = new Intent(this, OrderActivity.class);
+        EditText editText1 = findViewById(R.id.editText1);
+        EditText editText2 = findViewById(R.id.editText2);
+        EditText editText3 = findViewById(R.id.editText3);
+        String message = editText1.getText().toString() + ", " + editText2.getText().toString() + " & "  + editText3.getText().toString();
+        intent.putExtra(MSG, message);
+        startActivity(intent);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
